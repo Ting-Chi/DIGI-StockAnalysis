062d4b4fef8c77566983374329ef11e0768a526d
Ting Chi <kelly014026@gmail.com>
2019-12-13 16:53:39 +0800

    Add files via upload

---
+++ stock/stock/urls.py
@@ -0,0 +1,31 @@
+"""stock URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from main.views import hello_view
+from main.views import stock
+from main.views import corr
+from main.views import health
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', hello_view, name='hello_view'),
+    url(r'^stock/$', stock, name='stock'),
+    url(r'^corr/$', corr, name='corr'),
+    url(r'^health/$', health, name='health'),
+]
